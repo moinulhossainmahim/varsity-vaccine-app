4950d88f74e8e388fa8dc15fbdb73887e913fe16
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-04-09 01:31:52 +0600

    added utility functions

--- vaccine-app copy.c
+++ vaccine-app copy.c
@@ -23,38 +23,52 @@ int createRandomID();
 currentDate getCurrentDate();
 user takeUserInfo();
 void handleVaccinated();
-void PrintNextDoseDate(user users[], int id);
-void handleRegistration(user users[], int i);
+void PrintNextDoseDate(user users[100], int id);
+
 
 int main() {
-  int i = 0;
-  while(i<100) {
+  for(int i = 0; i < 100; i++) {
+    printf("TEST i: %d\n", i);
     char isInterested;
+    printf("TEST interest: %c\n", isInterested);
     printf("Are You interested to get vaccine? [Y/N]: ");
-
-    scanf(" %c", &isInterested);
-
-    if (isInterested == 'Y' || isInterested == 'y') {
+    scanf("%c", &isInterested);
+    if(isInterested == 'Y' || isInterested == 'y') 
+    {
       char isVaccinated;
       printf("Are You Vaccinated? [Y/N]: ");
       scanf(" %c", &isVaccinated);
-
-      if(isVaccinated == 'Y' || isVaccinated == 'y') {
+      if(isVaccinated == 'Y' || isVaccinated == 'y')
+      {
         handleVaccinated();
-      } else {
-        handleRegistration(userList, i);
       }
-
-    } else if (isInterested == 'N' || isInterested == 'n') {
-      printf("\033[1;31m");
-      printf("Take vaccine is must.You should take it as soon as possible.\n");
+      else
+      {
+        userList[i] = takeUserInfo();
+        if(userList[i].gender[0] == 'F' || userList[i].gender[0] == 'f') {
+          char isPregnant;
+          printf("Are You Pregnant? [Y/N]: ");
+          scanf(" %c", &isPregnant);
+          if(isPregnant == 'Y' || isPregnant == 'y') {
+            printf("SORRY YOU ARE NOT ELIGIBLE TO TAKE VACCINE\n");
+            return 0;
+          } else {
+            printf("Thank you for completing Vaccine registration.\nPlease Remember your ID");
+            printf("\nID: %d \nName: %s \nAge: %d \nGender: %s \nDate: %02d-%02d-%02d\n", userList[i].id, userList[i].name, userList[i].age, userList[i].gender, userList[i].date.day, userList[i].date.month, userList[i].date.year);
+          } 
+        }
+      }
+    } 
+    else if(isInterested == 'N' || isInterested == 'n') 
+    {
+      printf("Take vaccine is must.You should take it as soon as possible.");
       return 0;
     }
   }
   return 0;
 }
 
-// Utility Functions
+// Utiliy Functions
 int createRandomID() {
   srand(time(0));
   int randomId = rand() % 500;
@@ -99,7 +113,7 @@ void handleVaccinated() {
         break;
       case 2:
         printf("THANK YOU FOR TAKING VACCINE!\n");
-        return;
+        exit;
       default:
         printf("PLEASE GIVE VALID INFO.THANK YOU\n");
     }
@@ -118,23 +132,4 @@ void PrintNextDoseDate(user users[100], int id) {
   if(!isUserExists) {
     printf("THERE IS NO USER WITH THIS ID\n");
   }
-}
-
-void handleRegistration(user users[], int i) {
-  userList[i] = takeUserInfo();
-  if (userList[i].gender[0] == 'F' || userList[i].gender[0] == 'f') {
-    char isPregnant;
-    printf("Are You Pregnant? [Y/N]: ");
-    scanf(" %c", &isPregnant);
-    if(isPregnant == 'Y' || isPregnant == 'y') {
-      printf("SORRY YOU ARE NOT ELIGIBLE TO TAKE VACCINE\n");
-      return;
-    }
-  }
-  printf("Thank you for completing First Dose Vaccine Registratione ✅\n");
-  printf("\033[0;33m");
-  printf("Please Remember your ID\n");
-  printf("\033[0m");
-  printf("Here is Your Registration Info");
-  printf("\nID: %d \nName: %s \nAge: %d \nGender: %s \nDate: %02d-%02d-%02d\n", userList[i].id, userList[i].name, userList[i].age, userList[i].gender, userList[i].date.day, userList[i].date.month, userList[i].date.year);
 }
